00e5fb6dffd6cdf1961f93055a74cb8dfd37a210
Keane <KeaneCarotenuto@gmail.com>
2022-09-21 04:21:47 +1200

    Adverts in engine
    Kinda working on mobile, buttons dont update

---
+++ Assets/InterstitialAdExample.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+ 
+public class InterstitialAdExample : MonoBehaviour, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    [SerializeField] Button _showAdButton;
+    [SerializeField] string _androidAdUnitId = "Interstitial_Android";
+    [SerializeField] string _iOsAdUnitId = "Interstitial_iOS";
+    string _adUnitId;
+ 
+    void Awake()
+    {
+        // Get the Ad Unit ID for the current platform:
+        _adUnitId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOsAdUnitId
+            : _androidAdUnitId;
+
+        //Disable the button until the ad is ready to show:
+        _showAdButton.interactable = false;
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            LoadAd();
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            ShowAd();
+        }
+    }
+ 
+    // Load content to the Ad Unit:
+    public void LoadAd()
+    {
+        // IMPORTANT! Only load content AFTER initialization (in this example, initialization is handled in a different script).
+        Debug.Log("Loading Ad: " + _adUnitId);
+        Advertisement.Load(_adUnitId, this);
+    }
+ 
+    // Show the loaded content in the Ad Unit:
+    public void ShowAd()
+    {
+        // Disable the button:
+        _showAdButton.interactable = false;
+        // Note that if the ad content wasn't previously loaded, this method will fail
+        Debug.Log("Showing Ad: " + _adUnitId);
+        Advertisement.Show(_adUnitId, this);
+    }
+ 
+    // Implement Load Listener and Show Listener interface methods: 
+    public void OnUnityAdsAdLoaded(string adUnitId)
+    {
+        Debug.Log("Ad Loaded: " + adUnitId);
+ 
+        if (adUnitId.Equals(_adUnitId))
+        {
+            // Configure the button to call the ShowAd() method when clicked:
+            _showAdButton.onClick.AddListener(ShowAd);
+            // Enable the button for users to click:
+            _showAdButton.interactable = true;
+        }
+    }
+ 
+    public void OnUnityAdsFailedToLoad(string adUnitId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Error loading Ad Unit: {adUnitId} - {error.ToString()} - {message}");
+        // Optionally execute code if the Ad Unit fails to load, such as attempting to try again.
+    }
+ 
+    public void OnUnityAdsShowFailure(string adUnitId, UnityAdsShowError error, string message)
+    {
+        Debug.Log($"Error showing Ad Unit {adUnitId}: {error.ToString()} - {message}");
+        // Optionally execute code if the Ad Unit fails to show, such as loading another ad.
+    }
+ 
+    public void OnUnityAdsShowStart(string adUnitId) { }
+    public void OnUnityAdsShowClick(string adUnitId) { }
+    public void OnUnityAdsShowComplete(string adUnitId, UnityAdsShowCompletionState showCompletionState) { }
+}
